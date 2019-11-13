1811c33e808e68dfbb7a0a0441c045809f1bba55
Musinda Kadhuwa <kadhuwa.musinda@andela.com>
2019-11-13 23:19:38 +0200

    edit start script

--- src/index.js
+++ src/index.js
@@ -1,49 +1,35 @@
 /* eslint-disable no-console */
-import express from 'express';
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
-import passport from 'passport';
-import session from 'express-session';
-import cors from 'cors';
-import { config } from 'dotenv';
-import db from './sequelize/models';
-import router from './routes';
-import passConfig from './config/passport/passport';
-
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import passport from "passport";
+import cors from "cors";
+import { config } from "dotenv";
+import db from "./sequelize/models";
+import router from "./routes";
+import passConfig from "./config/passport/passport";
 
 config();
 
 const app = express();
-app.use(
-  cors({
-    origin: `${process.env.APP_FRONTEND}`
-  })
-);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 passConfig(passport);
-app.use(
-  session({
-    secret: process.env.SESSION_KEY,
-    resave: true,
-    saveUninitialized: true
-  })
-);
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
+app.use("/api/v1", router);
 
-app.use('/api/v1', router);
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
-    message: 'Welcome to UniNet Blogs System'
+    message: "Welcome to UniNet Blogs System"
   });
 });
 
 app.use((req, res) => {
-  res.status(404).json({ error: 'route not found' });
+  res.status(404).json({ error: "route not found" });
 });
 
 app.use((error, req, res, next) => {
@@ -56,7 +42,7 @@ app.use((error, req, res, next) => {
 const PORT = process.env.PORT || 3000;
 
 db.sequelize.sync({ alter: false }).then(() => {
-  console.log('Database Connected!');
+  console.log("Database Connected!");
   app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
   });
